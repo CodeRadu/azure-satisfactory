701df6666d8ab9f5973fd49a7872fe6c839af3e2
CodeRadu <47945947+CodeRadu@users.noreply.github.com>
2024-04-14 15:34:19 +0300

    Initial commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,40 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version = "3.99.0"
+  hashes = [
+    "h1:b24Yw8/EneYHRV3aPoVexmw0Eo252ur30tN+sPajzXk=",
+    "zh:20581c1f4c586a37af45ed4c2a86ff4d868cee79139a755bd29750d804cee3ef",
+    "zh:28b3cc4e5f8bc65a595eab011d5965203a39e92aa9e26df842ffc979305ac823",
+    "zh:4cb167f8bb82f9065b7b50d012be3045fce3c699b0ea0e257ad1995441227f72",
+    "zh:6fa5c6fa430921a4e0fe8d44eaf12210fb90afdf3f83cedfde1c691ae36e953c",
+    "zh:75eff5b0ea9fca46ed5a0425c5e33fbda470e6448917817e80ae898688568665",
+    "zh:9af0aeaa74bfc764c60eec7d212d31deb70e03e970d22449f11170f75108f9cf",
+    "zh:b5055767199a2927d41b543a16e905c1e0b209f14a2144c756786194e133b41d",
+    "zh:c3e30b0eed068a148498ac78a9e013bc2eef0eb3cc3b4484f77421d64a797dc2",
+    "zh:ce87cd35cef9e5805f921978a91a7a4e139e8cbc7674a94076cb1a20a0c2feb1",
+    "zh:d87b84f144c865145bd10093ead99b653ea363fd4e7315675727659ca78544d0",
+    "zh:ee5900a50d69e046aab6581f6d888014b3f8d543e5b17c50761579d3370935f2",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version = "4.0.5"
+  hashes = [
+    "h1:jb/Rg9inGYp4t8HtBoETESsQJgdmOHoe1bzzg2uNB3w=",
+    "zh:01cfb11cb74654c003f6d4e32bbef8f5969ee2856394a96d127da4949c65153e",
+    "zh:0472ea1574026aa1e8ca82bb6df2c40cd0478e9336b7a8a64e652119a2fa4f32",
+    "zh:1a8ddba2b1550c5d02003ea5d6cdda2eef6870ece86c5619f33edd699c9dc14b",
+    "zh:1e3bb505c000adb12cdf60af5b08f0ed68bc3955b0d4d4a126db5ca4d429eb4a",
+    "zh:6636401b2463c25e03e68a6b786acf91a311c78444b1dc4f97c539f9f78de22a",
+    "zh:76858f9d8b460e7b2a338c477671d07286b0d287fd2d2e3214030ae8f61dd56e",
+    "zh:a13b69fb43cb8746793b3069c4d897bb18f454290b496f19d03c3387d1c9a2dc",
+    "zh:a90ca81bb9bb509063b736842250ecff0f886a91baae8de65c8430168001dad9",
+    "zh:c4de401395936e41234f1956ebadbd2ed9f414e6908f27d578614aaa529870d4",
+    "zh:c657e121af8fde19964482997f0de2d5173217274f6997e16389e7707ed8ece8",
+    "zh:d68b07a67fbd604c38ec9733069fbf23441436fecf554de6c75c032f82e1ef19",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,297 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group."
+}
+
+variable "vm_type" {
+  type        = string
+  default     = "Standard_B2als_v2"
+  description = "The type of virtual machine."
+}
+
+variable "location" {
+  type        = string
+  default     = "westeurope"
+  description = "The location where the resources will be deployed."
+}
+
+variable "username" {
+  type        = string
+  description = "The username for the virtual machine."
+}
+
+variable "public_key" {
+  type        = string
+  description = "The public key for SSH authentication."
+}
+
+variable "start_time" {
+  type        = string
+  description = "The time when the virtual machine will start."
+}
+
+variable "stop_time" {
+  type        = string
+  description = "The time when the virtual machine will stop."
+}
+
+variable "timezone" {
+  type        = string
+  default     = "UTC"
+  description = "The timezone for the schedule."
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Provisioned = true
+  }
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "network"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "public_ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSatisfactory"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["7777", "15000", "15777"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  ip_configuration {
+    name                          = "Internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "tls_private_key" "admin" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name           = "satisfactory"
+  admin_username = "adminuser"
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.admin.public_key_openssh
+  }
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = var.location
+  network_interface_ids = [azurerm_network_interface.main.id]
+  size                  = var.vm_type
+  os_disk {
+    name                 = "os"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  priority        = "Spot"
+  eviction_policy = "Deallocate"
+  user_data = base64encode(templatefile("config.yaml", {
+    username   = var.username
+    public_key = var.public_key
+  }))
+}
+
+resource "azurerm_managed_disk" "home" {
+  name                 = "home"
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.main.name
+  create_option        = "Empty"
+  storage_account_type = "Standard_LRS"
+  disk_size_gb         = "16"
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "home" {
+  managed_disk_id    = azurerm_managed_disk.home.id
+  virtual_machine_id = azurerm_linux_virtual_machine.main.id
+  lun                = 10
+  caching            = "ReadWrite"
+}
+
+data "azurerm_subscription" "primary" {}
+
+resource "azurerm_user_assigned_identity" "subscription" {
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  name                = "uaid"
+}
+
+resource "azurerm_role_assignment" "uami" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.subscription.principal_id
+}
+
+resource "azurerm_automation_account" "scheduler" {
+  name                = "vm-scheduler"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "Basic"
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.subscription.id]
+  }
+}
+
+resource "azurerm_automation_runbook" "vm_power" {
+  name                    = "vm-power-runbook"
+  location                = var.location
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.scheduler.name
+  runbook_type            = "PowerShell"
+  content                 = <<EOF
+    Param(
+      [string]$powerstate
+    )
+
+    $resourcegroupname = "${azurerm_resource_group.main.name}"
+    $vmname            = "${azurerm_linux_virtual_machine.main.name}"
+    $uami              = "${azurerm_user_assigned_identity.subscription.client_id}"
+    $automationaccount = "${azurerm_automation_account.scheduler.name}"
+
+    $null = Disable-AzContextAutosave -Scope Process
+   
+    $AzureConnection = (Connect-AzAccount -Identity -AccountId $uami).context
+
+    $AzureConnection
+    $AzureContext = Set-AzContext -SubscriptionName $AzureConnection.Subscription -DefaultProfile $AzureConnection
+
+    if ($powerstate -eq "start") {
+      Start-AzVM -ResourceGroupName $resourcegroupname -Name $vmname -DefaultProfile $AzureContext
+    } elseif ($powerstate -eq "stop") {
+      Stop-AzVM -ResourceGroupName $resourcegroupname -Name $vmname -Force -DefaultProfile $AzureContext
+    }
+  EOF
+
+  log_verbose  = false
+  log_progress = true
+}
+
+resource "azurerm_automation_schedule" "start_vm" {
+  name                    = "start-vm-schedule"
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.scheduler.name
+  frequency               = "Day"
+  start_time              = var.start_time
+  timezone                = var.timezone
+}
+
+resource "azurerm_automation_job_schedule" "start_vm" {
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.scheduler.name
+  runbook_name            = azurerm_automation_runbook.vm_power.name
+  schedule_name           = azurerm_automation_schedule.start_vm.name
+
+  parameters = {
+    powerstate = "start"
+  }
+}
+
+resource "azurerm_automation_schedule" "stop_vm" {
+  name                    = "stop-vm-schedule"
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.scheduler.name
+  frequency               = "Day"
+  start_time              = var.stop_time
+  timezone                = var.timezone
+}
+
+resource "azurerm_automation_job_schedule" "stop_vm" {
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.scheduler.name
+  runbook_name            = azurerm_automation_runbook.vm_power.name
+  schedule_name           = azurerm_automation_schedule.stop_vm.name
+
+  parameters = {
+    powerstate = "stop"
+  }
+}
+
+output "username" {
+  value = var.username
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.main.ip_address
+}
+
+output "start_time" {
+  value = formatdate("hh:mm", azurerm_automation_schedule.start_vm.start_time)
+}
+
+output "stop_time" {
+  value = formatdate("hh:mm", azurerm_automation_schedule.stop_vm.start_time)
+}
